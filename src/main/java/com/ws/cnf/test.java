7cbeb47bebecf750527d6da21287e3a42383b306
jeff <123033590@qq.com>
2022-04-23 20:53:38 +0800

    2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:2:
    Signed-off-by: jeff <123033590@qq.com>

--- src/main/java/com/ws/cnf/test.java
+++ src/main/java/com/ws/cnf/test.java
@@ -4,10 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("xxxx");
-        System.out.println("xxxx222");
-        System.out.println("xxxx");
-        System.out.println("xxxx222");
-        System.out.println("xxxx3333");
+        System.out.println("xxx111x");
 
 
     }
