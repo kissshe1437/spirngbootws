a92044afb459139a6a0bd725552f2477d0bf8224
jeff <123033590@qq.com>
2022-04-23 20:49:42 +0800

    xxx

--- src/main/java/com/ws/cnf/test.java
+++ src/main/java/com/ws/cnf/test.java
@@ -4,5 +4,10 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("xxxx");
+        System.out.println("xxxx222");
+        System.out.println("xxxx");
+        System.out.println("xxxx222");
+
+
     }
 }
